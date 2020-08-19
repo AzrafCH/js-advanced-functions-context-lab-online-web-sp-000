42dfd7b16ea33a4987fcb6000f3d456b4e0322f1
Azraf Chowdhury <achowdhur@ltu.edu>
2020-08-19 17:37:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,67 +8,71 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
- function createEmployeeRecord(arr) {
-   let obj = {
-     firstName: arr[0],
-     familyName: arr[1],
-     title: arr[2],
-     payPerHour: arr[3],
-     timeInEvents: [],
-     timeOutEvents: []
-   }
-   return obj
+ let createEmployeeRecord = function (info) {
+     return {
+         firstName: info[0], 
+         familyName: info[1],
+         title: info[2], 
+         payPerHour: info[3], 
+         timeInEvents: [],
+         timeOutEvents: []
+     }
  }
-
- function createEmployeeRecords(arrayOfArrays) {
-   let theArray = [];
-   arrayOfArrays.forEach(element => {
-     theArray.push(createEmployeeRecord(element))
-   });
-   return theArray
+ 
+ let createEmployeeRecords = function (arr) {
+    return arr.map(function(r) {
+         return createEmployeeRecord(r)
+     })
  }
-
- function createTimeInEvent(obj, timeStamp) {
-   let hour = parseInt(timeStamp.split(' ')[1]);
-   let date = timeStamp.split(' ')[0];
-   obj.timeInEvents.push({type: "TimeIn", hour: hour, date: date})
-   return obj
+ 
+ let createTimeInEvent = function (stamp) {
+     let [date, hour] = stamp.split(' ')
+     this.timeInEvents.push({
+         type: "TimeIn",
+         hour: parseInt(hour, 10), 
+         date: date
+     })
+     return this
  }
-
- function createTimeOutEvent(obj, timeStamp) {
-   let hour = parseInt(timeStamp.split(' ')[1]);
-   let date = timeStamp.split(' ')[0];
-   obj.timeOutEvents.push({type: "TimeOut", hour: hour, date: date})
-   return obj
+ 
+ let createTimeOutEvent = function (stamp) {
+     let [date, hour] = stamp.split(' ')
+     this.timeOutEvents.push({
+         type: "TimeOut",
+         hour: parseInt(hour, 10), 
+         date: date
+     })
+     return this
  }
-
- function hoursWorkedOnDate(obj, timeStamp) {
-   let timeIn = obj.timeInEvents.find(x => x.date === timeStamp)
-   let timeOut = obj.timeOutEvents.find(x => x.date === timeStamp)
-   let result = (timeOut.hour - timeIn.hour) / 100
-   return result
+ 
+ let hoursWorkedOnDate = function (stamp) { 
+     let inTime = this.timeInEvents.find(function(e){
+         return e.date === stamp
+     })
+ 
+     let outTime = this.timeOutEvents.find(function(e){
+        return e.date === stamp
+     })
+ 
+     return (outTime.hour - inTime.hour) / 100
  }
-
- function wagesEarnedOnDate(obj, timeStamp) {
-   return hoursWorkedOnDate(obj, timeStamp) * obj.payPerHour
+ 
+ let wagesEarnedOnDate = function(date) {
+     let rate = this.payPerHour
+     let hours = hoursWorkedOnDate.call(this,date)
+     return rate * hours
  }
-
- function allWagesFor(obj) {
-   let eligibleDates = obj.timeInEvents.map(function(e){
-      return e.date
-    })
-
-  let payable = eligibleDates.reduce(function(memo, d){
-      return memo + wagesEarnedOnDate(obj, d)
-    }, 0)
-  return payable
+ 
+ let calculatePayroll = function(arr) {
+     let payroll = arr.reduce(function(total, rec) {
+         return allWagesFor.call(rec) + total
+     }, 0)
+     return payroll
  }
-
- function calculatePayroll(array) {
-   let sum = array.map((e) => allWagesFor(e))
-   return sum.reduce((num, sum) => num + sum)
- }
-
- function findEmployeeByFirstName(srcArray, firstName) {
-   return srcArray.find(x => {return x.firstName === firstName})
+ 
+ let findEmployeeByFirstName = function (arr, name) {
+     let employee = arr.find(function(e) {
+         return e.firstName === name
+     })
+     return employee
  }
