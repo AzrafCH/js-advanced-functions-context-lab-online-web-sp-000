2ff66d053f29d7fc9c94c9683df5f6b219b0939f
Azraf Chowdhury <achowdhur@ltu.edu>
2020-08-19 17:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,86 +8,67 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
-
- let createEmployeeRecord = function(row){
-     return {
-         firstName: row[0],
-         familyName: row[1],
-         title: row[2],
-         payPerHour: row[3],
-         timeInEvents: [],
-         timeOutEvents: []
-     }
+ function createEmployeeRecord(arr) {
+   let obj = {
+     firstName: arr[0],
+     familyName: arr[1],
+     title: arr[2],
+     payPerHour: arr[3],
+     timeInEvents: [],
+     timeOutEvents: []
+   }
+   return obj
  }
 
- let createEmployeeRecords = function(employeeRowData) {
-     return employeeRowData.map(function(row){
-         return createEmployeeRecord(row)
-     })
+ function createEmployeeRecords(arrayOfArrays) {
+   let theArray = [];
+   arrayOfArrays.forEach(element => {
+     theArray.push(createEmployeeRecord(element))
+   });
+   return theArray
  }
 
- let createTimeInEvent = function(employee, dateStamp){
-     let [date, hour] = dateStamp.split(' ')
-
-     employee.timeInEvents.push({
-         type: "TimeIn",
-         hour: parseInt(hour, 10),
-         date,
-     })
-
-     return employee
+ function createTimeInEvent(obj, timeStamp) {
+   let hour = parseInt(timeStamp.split(' ')[1]);
+   let date = timeStamp.split(' ')[0];
+   obj.timeInEvents.push({type: "TimeIn", hour: hour, date: date})
+   return obj
  }
 
- let createTimeOutEvent = function(employee, dateStamp){
-     let [date, hour] = dateStamp.split(' ')
-
-     employee.timeOutEvents.push({
-         type: "TimeOut",
-         hour: parseInt(hour, 10),
-         date,
-     })
-
-     return employee
+ function createTimeOutEvent(obj, timeStamp) {
+   let hour = parseInt(timeStamp.split(' ')[1]);
+   let date = timeStamp.split(' ')[0];
+   obj.timeOutEvents.push({type: "TimeOut", hour: hour, date: date})
+   return obj
  }
 
- let hoursWorkedOnDate = function(employee, soughtDate){
-     let inEvent = employee.timeInEvents.find(function(e){
-         return e.date === soughtDate
-     })
-
-     let outEvent = employee.timeOutEvents.find(function(e){
-         return e.date === soughtDate
-     })
-
-     return (outEvent.hour - inEvent.hour) / 100
+ function hoursWorkedOnDate(obj, timeStamp) {
+   let timeIn = obj.timeInEvents.find(x => x.date === timeStamp)
+   let timeOut = obj.timeOutEvents.find(x => x.date === timeStamp)
+   let result = (timeOut.hour - timeIn.hour) / 100
+   return result
  }
 
- let wagesEarnedOnDate = function(employee, dateSought){
-     let rawWage = hoursWorkedOnDate(employee, dateSought)
-         * employee.payPerHour
-     return parseFloat(rawWage.toString())
+ function wagesEarnedOnDate(obj, timeStamp) {
+   return hoursWorkedOnDate(obj, timeStamp) * obj.payPerHour
  }
 
- let allWagesFor = function(employee){
-     let eligibleDates = employee.timeInEvents.map(function(e){
-         return e.date
-     })
+ function allWagesFor(obj) {
+   let eligibleDates = obj.timeInEvents.map(function(e){
+      return e.date
+    })
 
-     let payable = eligibleDates.reduce(function(memo, d){
-         return memo + wagesEarnedOnDate(employee, d)
-     }, 0)
-
-     return payable
+  let payable = eligibleDates.reduce(function(memo, d){
+      return memo + wagesEarnedOnDate(obj, d)
+    }, 0)
+  return payable
  }
 
- let findEmployeeByFirstName = function(srcArray, firstName) {
-   return srcArray.find(function(rec){
-     return rec.firstName === firstName
-   })
+ function calculatePayroll(array) {
+   let sum = array.map((e) => allWagesFor(e))
+   return sum.reduce((num, sum) => num + sum)
  }
 
- let calculatePayroll = function(arrayOfEmployeeRecords){
-     return arrayOfEmployeeRecords.reduce(function(memo, rec){
-         return memo + allWagesFor(rec)
-     }, 0)
+ function findEmployeeByFirstName(srcArray, firstName) {
+   return srcArray.find(x => {return x.firstName === firstName})
  }
