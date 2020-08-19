53427e62cfc6938f4cfecd00feaf2ee0aafcd37f
Azraf Chowdhury <achowdhur@ltu.edu>
2020-08-19 17:38:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -76,3 +76,13 @@
      })
      return employee
  }
+
+ let allWagesFor = function () {
+     let eligibleDates = this.timeInEvents.map(function (e) {
+         return e.date
+     })
+     let payable = eligibleDates.reduce(function (memo, d) {
+         return memo + wagesEarnedOnDate.call(this, d)
+     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+     return payable
+ }
